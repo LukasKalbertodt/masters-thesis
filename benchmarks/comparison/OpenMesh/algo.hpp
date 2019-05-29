c3bd7b5057a108714b1109b6e303208acc4eedc7
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-05-29 22:03:58 +0200

    Make OpenMesh algo benchmarks more idiomatic
    From quick tests it seems like the speed didn't change at all.

--- benchmarks/comparison/OpenMesh/algo.hpp
+++ benchmarks/comparison/OpenMesh/algo.hpp
@@ -1,5 +1,6 @@
 #include <OpenMesh/Core/IO/MeshIO.hh>
 #include <OpenMesh/Tools/Subdivider/Uniform/Sqrt3T.hh>
+#include <OpenMesh/Core/Utils/PropertyManager.hh>
 
 // ===== Smoothing =============================================================================
 Outcome smooth_bench(const char* filename) {
@@ -15,31 +16,22 @@ Outcome smooth_bench(const char* filename) {
         // The following code is taken from the OpenMesh tutorial and adjusted!
         // --------------------------------------------------------------------
         // this vector stores the computed centers of gravity
-        std::vector<MyMesh::Point>  cogs;
-        std::vector<MyMesh::Point>::iterator cog_it;
-        cogs.reserve(mesh.n_vertices());
+        auto cogs = OpenMesh::makeTemporaryProperty<OpenMesh::VertexHandle, MyMesh::Point>(mesh);
 
-        // smoothing mesh argv[1] times
-        MyMesh::VertexIter          v_it, v_end(mesh.vertices_end());
-        MyMesh::VertexVertexIter    vv_it;
-        MyMesh::Point               cog;
-        MyMesh::Scalar              valence;
-
-        for (v_it=mesh.vertices_begin(); v_it!=v_end; ++v_it) {
-            cog[0] = cog[1] = cog[2] = valence = 0.0;
-
-            for (vv_it=mesh.vv_iter( *v_it ); vv_it.is_valid(); ++vv_it) {
-                cog += mesh.point( *vv_it );
+        for (const auto& vh : mesh.vertices()) {
+            cogs[vh] = {0, 0, 0};
+            int valence = 0;
+            // Iterate over all 1-ring vertices around vh
+            for (const auto& vvh : mesh.vv_range(vh)) {
+                cogs[vh] += mesh.point(vvh);
                 ++valence;
             }
-            cogs.push_back(cog / valence);
+            cogs[vh] /= valence;
         }
 
-        for (v_it=mesh.vertices_begin(), cog_it=cogs.begin();
-             v_it!=v_end; ++v_it, ++cog_it) {
-            if ( !mesh.is_boundary( *v_it ) ) {
-                mesh.set_point( *v_it, *cog_it );
-            }
+        // Move all vertices to the previously computed positions
+        for (const auto& vh : mesh.vertices()) {
+            mesh.point(vh) = cogs[vh];
         }
         // --------------------------------------------------------------------
 
@@ -89,9 +81,8 @@ Outcome boundary_count_bench(const char* filename) {
     const auto& mesh = original;
     return run_bench([&mesh]() {
         size_t count = 0;
-        auto v_end = mesh.vertices_end();
-        for (auto v_it = mesh.vertices_begin(); v_it != v_end; ++v_it) {
-            if (!mesh.is_boundary( *v_it )) {
+        for (const auto& vh: mesh.vertices()) {
+            if (!mesh.is_boundary(vh)) {
                 count += 1;
             }
         }
@@ -121,9 +112,8 @@ Outcome calc_normals_bench(const char* filename) {
         std::vector<MyMesh::Point> normals;
         normals.reserve(mesh.n_faces());
 
-        auto f_end = mesh.faces_end();
-        for (auto f_it = mesh.faces_begin(); f_it != f_end; ++f_it) {
-            normals.push_back(mesh.calc_face_normal(*f_it));
+        for (const auto& fh: mesh.faces()) {
+            normals.push_back(mesh.calc_face_normal(fh));
         }
 
         return normals;
