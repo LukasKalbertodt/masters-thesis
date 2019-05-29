87d59df563f603336a9b0a40e331ddb8607e7f73
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-05-29 15:06:57 +0200

    Add PMP algo benchmarks

---
+++ benchmarks/comparison/pmp/algo.hpp
@@ -0,0 +1,135 @@
+#include <pmp/algorithms/SurfaceSubdivision.h>
+#include <pmp/MatVec.h>
+#include <pmp/algorithms/SurfaceNormals.h>
+
+using pmp::SurfaceMesh;
+using pmp::SurfaceSubdivision;
+using pmp::Normal;
+using pmp::Point;
+using pmp::IOFlags;
+using pmp::vec3;
+
+
+
+// ===== Smoothing =============================================================================
+Outcome smooth_bench(const char* file) {
+    SurfaceMesh original;
+    original.read(file);
+
+
+    return run_bench([&original]() {
+        auto mesh = SurfaceMesh(original);
+
+        auto new_pos = mesh.add_vertex_property<Point>("v:new_pos");
+        auto points = mesh.get_vertex_property<Point>("v:point");
+
+        auto vertices = vector<Point>();
+        for (auto v: mesh.vertices()) {
+            if (mesh.is_boundary(v)) {
+                new_pos[v] = points[v];
+            } else {
+                auto total_displacement = vec3(0, 0, 0);
+                size_t valence = 0;
+
+                for (auto neighbor: mesh.vertices(v)) {
+                    total_displacement += points[neighbor];
+                }
+
+                new_pos[v] = total_displacement / valence;
+            }
+        }
+
+        for (auto v: mesh.vertices()) {
+            points[v] = new_pos[v];
+        }
+
+        mesh.remove_vertex_property(new_pos);
+
+        return mesh;
+    });
+}
+
+
+#define GEN_SMOOTH_BENCH(name, mesh) Outcome name() {       \
+    return smooth_bench("../../../data/" mesh);     \
+}
+
+GEN_SMOOTH_BENCH(algo_smooth_cat, "cat.ply")
+GEN_SMOOTH_BENCH(algo_smooth_tiger, "tiger.ply")
+
+
+// ===== SQRT3 =============================================================================
+Outcome sqrt3_bench(const char* file) {
+    SurfaceMesh original;
+    original.read(file);
+
+    return run_bench([&original]() {
+        auto mesh = SurfaceMesh(original);
+        auto sub = SurfaceSubdivision(mesh);
+        sub.sqrt3();
+
+        return mesh;
+    });
+}
+
+#define GEN_SQRT3_BENCH(name, mesh) Outcome name() {       \
+    return sqrt3_bench("../../../data/" mesh);     \
+}
+
+GEN_SQRT3_BENCH(algo_sqrt3_cat, "cat.ply")
+GEN_SQRT3_BENCH(algo_sqrt3_tiger, "tiger.ply")
+
+
+
+// ===== Boundary Count ========================================================================
+Outcome boundary_count_bench(const char* file) {
+    SurfaceMesh original;
+    original.read(file);
+
+    const auto& mesh = original;
+    return run_bench([&mesh]() {
+        size_t count = 0;
+        for (auto v: mesh.vertices()) {
+            if (mesh.is_boundary(v)) {
+                count += 1;
+            }
+        }
+
+        return count;
+    });
+}
+
+#define GEN_BOUNDARY_COUNT_BENCH(name, mesh) Outcome name() {       \
+    return boundary_count_bench("../../../data/" mesh);     \
+}
+
+GEN_BOUNDARY_COUNT_BENCH(algo_boundary_count_cat, "cat.ply")
+GEN_BOUNDARY_COUNT_BENCH(algo_boundary_count_tiger, "tiger.ply")
+
+
+
+
+// ===== Normals ============================================================================
+Outcome calc_normals_bench(const char* file) {
+    SurfaceMesh original;
+    original.read(file);
+
+    const auto& mesh = original;
+    return run_bench([&mesh]() {
+        std::vector<Normal> normals;
+        normals.reserve(mesh.faces_size());
+
+        for (auto f: mesh.faces()) {
+            normals.push_back(pmp::SurfaceNormals::compute_face_normal(mesh, f));
+        }
+
+        return normals;
+    });
+}
+
+#define GEN_CALC_NORMALS_BENCH(name, mesh) Outcome name() {       \
+    return calc_normals_bench("../../../data/" mesh);     \
+}
+
+GEN_CALC_NORMALS_BENCH(algo_calc_normals_cat, "cat.ply")
+GEN_CALC_NORMALS_BENCH(algo_calc_normals_tiger, "tiger.ply")
--- benchmarks/comparison/pmp/main.cpp
+++ benchmarks/comparison/pmp/main.cpp
@@ -1,20 +1,36 @@
 #include <pmp/SurfaceMesh.h>
 #include "../util.hpp"
 
+#include "algo.hpp"
 #include "read_file.hpp"
 #include "write_file.hpp"
 
 
 int main(int argc, const char *argv[]) {
     run_all_benches(argc, argv, {
+        // Read
         BENCH(read_file_ply_cat),
         BENCH(read_file_stl_cat),
         BENCH(read_file_ply_tiger),
         BENCH(read_file_stl_tiger),
 
+        // Write
         BENCH(write_file_ply_cat),
         BENCH(write_file_stl_cat),
         BENCH(write_file_ply_tiger),
         BENCH(write_file_stl_tiger),
+
+        // Algorithms
+        BENCH(algo_smooth_cat),
+        BENCH(algo_smooth_tiger),
+
+        BENCH(algo_sqrt3_cat),
+        BENCH(algo_sqrt3_tiger),
+
+        BENCH(algo_boundary_count_cat),
+        BENCH(algo_boundary_count_tiger),
+
+        BENCH(algo_calc_normals_cat),
+        BENCH(algo_calc_normals_tiger),
     });
 }
